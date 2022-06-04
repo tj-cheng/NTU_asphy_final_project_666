c32da3754607140ae8669a29623d6a9cebea92d1
Ta-Jung Cheng <mko09ijn2012@gmail.com>
2022-06-04 19:35:34 +0800

    voronoi ---> voronoinearlabel.py (final version)

---
+++ voronoiNearLabel.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+points = np.array([[3, .2], [-2, 1], [0, 2.1], [1.1, -1], [1, 1], [0.9, 1.9],[2.2, -0.2], [1.9, 1.1], [2.1, 2.3]])
+
+from scipy.spatial import Voronoi, voronoi_plot_2d
+vor = Voronoi(points)
+label = 8
+
+print("points[4] =", points[label])
+#print("ridge_points=",vor.ridge_points[5])
+#print("ridge_vertics=",vor.ridge_vertices[5])
+print("region for point 4 =", vor.point_region[label])
+print("vertices for region for point 4 =", vor.regions[vor.point_region[label]])
+#print(points[vor.regions[vor.point_region[label]]])
+print("coordinate of Voronoi vertices associated with point 4:")
+print(vor.vertices[vor.regions[vor.point_region[label]]])
+nn = [ [] for i in range(9) ]
+for label in range(9):
+    edgevec=vor.vertices[vor.regions[vor.point_region[label]]]
+    numnode=len(vor.vertices[vor.regions[vor.point_region[label]]])
+    medge=[]
+    for i in range(numnode):
+        j = (i+1)%numnode
+        midP = (edgevec[i]+edgevec[j])/2.
+        medge.append(midP)
+            
+    for j in range(numnode):
+        mag = np.linalg.norm(points[label]-medge[j])
+        for k in range(9):
+            if(k != label):
+                mag1 = np.linalg.norm(points[k]-medge[j])
+                if(np.abs(mag-mag1) < 10**-5):
+                    box = mag1
+                    boxlabel = k
+                    
+        nn[label].append(boxlabel)
+        
+        print(points[label],boxlabel,points[boxlabel])
+
+print(nn)
+
+
+
+        
+        
+import matplotlib.pyplot as plt
+fig = voronoi_plot_2d(vor)
+ax = plt.gca()
+ax.set_aspect('equal', adjustable='box')
+
+plt.show()
+
